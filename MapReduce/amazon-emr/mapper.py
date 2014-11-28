86196364aae7d6bad4931a79e358d7fda8854929
emanuelvalente <emanuelvalente@gmail.com>
2014-11-27 20:54:45 -0700

    amazon emr code

---
+++ MapReduce/amazon-emr/mapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys, re
+
+def main(argv):
+	for line in sys.stdin:
+		line = line.strip()
+		#pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*") 
+		pattern = re.compile("\w+") 
+		for word in pattern.findall(line): 
+			line.replace(',', " ")
+			#words = line.split()
+			print(word.lower() + "\t" + "1")
+
+if __name__ == "__main__":
+	main(sys.argv)
+
---
+++ MapReduce/amazon-emr/reducer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import sys, re
+import collections
+
+def main(argv):
+
+	last_word = None
+	word_count = 0
+
+	#final_result = {}
+
+	for line in sys.stdin:
+		line = line.strip()
+
+		word, count = line.split("\t")
+
+		count = int(count)
+
+		#first time iteration
+		if not last_word:
+			last_word = word
+
+		#just count
+		if word == last_word:
+			word_count += count
+
+		#different word	
+		else:
+			#final_result[last_word] = word_count
+			result_to_print = [last_word, word_count]
+			#prints "word\t#occurs"
+			print("\t".join(str(v) for v in result_to_print))
+			#our "new" word
+			last_word = word
+ 			word_count = 1
+
+ 	'''
+ 	when we have just one reducer we can do this:
+    #prints until the range(nth) most
+    #common word 
+ 	for i in range(20):
+ 		mylist = sorted(final_result, key=final_result.__getitem__, reverse=True)		
+ 		print(mylist[i], final_result[mylist[i]])
+ 	'''	
+
+
+if __name__ == "__main__":
+	main(sys.argv)
+
